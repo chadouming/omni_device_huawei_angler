8fdb0c0a74695616e1976fff3df0fb3d019021f1
Chad Cormier Roussel <chadcormierroussel@gmail.com>
2016-03-09 21:34:10 +0100

    Update to my liking

--- omni_angler.mk
+++ omni_angler.mk
@@ -23,11 +23,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Override product naming for Omni
 PRODUCT_NAME := omni_angler
-PRODUCT_BRAND := google
+PRODUCT_BRAND := Google
 PRODUCT_MODEL := Nexus 6P
 PRODUCT_MANUFACTURER := HUAWEI
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BUILD_FINGERPRINT="google/angler/angler:6.0.1/MMB29M/2431559:user/release-keys" \
-    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29M 2431559 release-keys"
+    PRODUCT_NAME=angler \
+    BUILD_FINGERPRINT=google/angler/angler:6.0.1/MMB29Q/2480792:user/release-keys \
+    PRIVATE_BUILD_DESC="angler-user 6.0.1 MMB29Q 2480792 release-keys"
